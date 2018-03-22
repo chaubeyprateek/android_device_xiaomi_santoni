6e0fb24b76fd8d5770379694c07b609507fcf143
Prateek Chaubey <chaubeyprateek@gmail.com>
2018-03-22 06:44:39 +0100

    santoni: Add set_baseband script and corresponding sepolicy

---
+++ rootdir/etc/set_baseband.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+# grep the modem partition for baseband version and set it
+setprop gsm.version.baseband `strings /firmware/IMAGE/MODEM.B12| grep "MPSS.TA.*_V" | head -1`
+setprop ro.baseband.unknown 0
